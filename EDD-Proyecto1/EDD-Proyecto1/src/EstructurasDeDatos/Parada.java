b541a0cdbbec1a876a6e4ed3159cf3c3596672ac
Santiagocm15 <157600349+Santiagocm15@users.noreply.github.com>
2024-10-18 13:03:54 -0400

    Add files via upload

---
+++ EDD-Proyecto1/EDD-Proyecto1/src/EstructurasDeDatos/Parada.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EstructurasDeDatos;
+
+/**
+ * @version 13/10/2024
+ * @author Michelle García
+ */
+public class Parada {
+    
+    private String name;
+    private boolean sucursal;
+    private Lista adyacentes;
+
+    public Parada(String name) {
+        this.name = name;
+        this.sucursal = false;
+        this.adyacentes = null;
+    }
+
+    /**
+     * @return the sucursal
+     */
+    public boolean hasSucursal() {
+        return isSucursal();
+    }
+
+    /**
+     * @param sucursal the sucursal to set
+     */
+    public void setSucursal(boolean sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    /**
+     * @return the adyacentes
+     */
+    public Lista getAdyacentes() {
+        return adyacentes;
+    }
+
+    /**
+     * @param adyacentes the adyacentes to set
+     */
+    public void setAdyacentes(Lista adyacentes) {
+        this.adyacentes = adyacentes;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the sucursal
+     */
+    public boolean isSucursal() {
+        return sucursal;
+    }
+
+    /**
+     */
+    public void seleccionar_sucursal() {
+        this.setSucursal(true);
+    }
+
+    /**
+     */
+    public void quitar_sucursal() {
+        this.setSucursal(false);
+    }
+
+    /**
+     * @return
+     */
+    public String leer_info() {
+        String txt = "";
+        txt = txt + "\nNombre: " + getName();
+        return txt;
+    }
+
+}
---
+++ EDD-Proyecto1/EDD-Proyecto1/src/edd/proyecto1/EDDProyecto1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package edd.proyecto1;
+import EstructurasDeDatos.Grafo;
+import EstructurasDeDatos.Lista;
+import EstructurasDeDatos.Nodo;
+import EstructurasDeDatos.Parada;
+import edd.Interfaz.Menu;
+/**
+ *
+ * @author lalau
+ */
+public class EDDProyecto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu menuinicial= new Menu();
+        menuinicial.show();
+    }
+    
+}
