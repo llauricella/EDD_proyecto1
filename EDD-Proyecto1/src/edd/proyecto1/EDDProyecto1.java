33cc88834b3aebd4227db3b473c1b9009c6e0255
llauricella <l.lauricella@correo.unimet.edu.ve>
2024-10-25 17:36:44 -0400

    V 1.7

--- EDD-Proyecto1/src/edd/proyecto1/EDDProyecto1.java
+++ EDD-Proyecto1/src/edd/proyecto1/EDDProyecto1.java
@@ -3,18 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package edd.proyecto1;
+import edd.Interfaz.MenuControles;
 
 /**
- *
- * @author lalau
+ * @version 23/10/2024
+ * @author Luigi Lauricella
  */
 public class EDDProyecto1 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        // inicialización de la interfaz menú
+        MenuControles menuinicial= new MenuControles();
+        menuinicial.show();
     }
-    
 }
